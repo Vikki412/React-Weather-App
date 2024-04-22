764546da61c0fe0ff282c8adecb9287030b0d4a5
Vikki412 <167021735+Vikki412@users.noreply.github.com>
2024-04-22 16:00:56 +0300

    injected weatherForecast component

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,7 @@ import "./styles.css";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 
 export default function Weather(props) {
   const [city, setCity] = useState(props.city);
@@ -56,6 +57,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+        <WeatherForecast />
       </div>
     );
   } else {
