be565469ad00255617b787fe4d075d01886b1f98
Vikki412 <167021735+Vikki412@users.noreply.github.com>
2024-04-23 09:04:18 +0300

    injected Api

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,20 @@
 import React from "react";
+import axios from "axios";
 import WeatherIcon from "./WeatherIcon";
 import "./Forecast.css";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "97f8e93f00107773f88eafd933ce86b7";
+  let latitude = props.coordinates.lat;
+  let longitude = props.coordinates.lon;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
