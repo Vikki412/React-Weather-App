9bff751d719aff657009b65baa2d6482ead103f7
Vikki412 <167021735+Vikki412@users.noreply.github.com>
2024-04-23 12:00:14 +0300

    changed style and position of icon and temp

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,18 +6,30 @@ import WeatherIcon from "./WeatherIcon";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <WeatherIcon code={props.data.icon} size={90} />
-      <h1>
-        <WeatherTemperature celsius={props.data.temperature} />
-      </h1>
-      <h2>{props.data.city}</h2>
-      <h3>
+      <h1>{props.data.city}</h1>
+      <p>
         <FormattedDate date={props.data.date} />{" "}
-      </h3>
+      </p>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} size={60} />
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="clearfix">
+            <div className="float-left">
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
+        </div>
+      </div>
       <p>{props.data.description}</p>{" "}
       <div className="container">
         <div className="row">
-          <div className="col-6">
+          <div className="col-4">
             <ul>
               <li>
                 <strong>{props.data.humidity} %</strong>
@@ -25,12 +37,20 @@ export default function WeatherInfo(props) {
               <li>Humidity</li>
             </ul>
           </div>
-          <div className="col-6">
+          <div className="col-4">
             <ul>
               <li>
                 <strong>{props.data.wind} kmh</strong>
               </li>
               <li>Wind</li>
+            </ul>
+          </div>
+          <div className="col-4">
+            <ul>
+              <li>
+                <strong>{props.data.pressure} hpa</strong>
+              </li>
+              <li>Pressure</li>
             </ul>
           </div>
         </div>
